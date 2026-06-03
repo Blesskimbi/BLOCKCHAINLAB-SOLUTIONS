f17e9d3e29854b253ba514d3a0182f8bf2851e1f
Bleskimbi <blesskimbi439@gmail.com>
2026-06-03 20:38:10 +0100

    blec

--- contracts/LabToken.sol
+++ contracts/LabToken.sol
@@ -5,14 +5,14 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract LabToken is ERC20, Ownable {
-    uint256 public constant MAX_SUPPLY = 1_000_000 * 10**18;
+ uint256 public constant MAX_SUPPLY = 1_000_000 * 10**18;
 
-    constructor() ERC20("LabToken", "LTK") Ownable(msg.sender) {
-        _mint(msg.sender, 100_000 * 10**18);
-    }
+ constructor() ERC20("LabToken", "LTK") Ownable(msg.sender) {
+  _mint(msg.sender, 100_000 * 10**18);
+ }
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        require(totalSupply() + amount <= MAX_SUPPLY, "Exceeds max supply");
-        _mint(to, amount);
-    }
+ function mint(address to, uint256 amount) public onlyOwner {
+  require(totalSupply() + amount <= MAX_SUPPLY, "Exceeds max supply");
+  _mint(to, amount);
+ }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,9 +2,9 @@ const { ethers } = require("hardhat");
 
 async function main() {
     const Token = await ethers.getContractFactory("LabToken");
-    const token = await Token.deploy();
-    await token.waitForDeployment();
-    console.log("LabToken deployed to:", await token.getAddress());
+    const contract  = await Token.deploy();
+    await contract.waitForDeployment();
+    console.log("LabToken deployed to:", await contract.getAddress());
 }
 
 main().catch((err) => { console.error(err); process.exit(1); });
